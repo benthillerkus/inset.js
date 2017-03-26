f7745dafd886deba93aa56c93d4970115cfb34d2
Pat Lillis <lillispm@gmail.com>
2017-03-25 23:06:48 -0600

    add support for arbitrary shapes.

--- src/inset.js
+++ src/inset.js
@@ -6,10 +6,25 @@
   // Needed for the drawing of the insets.
   var fillRect = CanvasRenderingContext2D.prototype.fillRect
 
-  var replacements = ['fillRect']
+  // These need to have the shadow filled in.
+  var fillFunctions = ['fillRect', 'fill']
 
-  // Override the "fillRect" method.
-  function override (object, methodName, callback) {
+  // These should not be filled, and instead just called on the hidden canvas
+  // directly.
+  var passThroughFunctions = [
+    'beginPath',
+    'closePath',
+    'moveTo',
+    'lineTo',
+    'bezierCurveTo',
+    'quadraticCurveTo',
+    'arc',
+    'arcTo',
+    'rect'
+  ]
+
+  // Override the method method to call "callback" if shadowInset = true.
+  function overrideIfInset (object, methodName, callback) {
     var original = object[methodName]
     object[methodName] = function () {
       if (this.shadowInset === true) {
@@ -20,20 +35,37 @@
     }
   }
 
-  for (var i = 0; i < replacements.length; i++) {
-    var replacement = replacements[i]
-    override(
+  // Set up pass-through functions that just re-apply to the hidden canvas.
+  for (let i = 0; i < passThroughFunctions.length; i++) {
+    var passThroughFunction = passThroughFunctions[i]
+    overrideIfInset(
       CanvasRenderingContext2D.prototype,
-      replacement,
+      passThroughFunction,
+      function (otherCtx, original, args) {
+        // Reset canvas size, if necessary.
+        var canvas = otherCtx.canvas
+        var buffer = Math.max(canvas.width, canvas.height)
+        resetSize(c, canvas, buffer)
+
+        // Apply function to original and hidden canvases.
+        original.apply(ctx, args)
+        original.apply(otherCtx, args)
+      }
+    )
+  }
+
+  // Set up fill functions to actually draw the shadow.
+  for (let i = 0; i < fillFunctions.length; i++) {
+    var fillFunction = fillFunctions[i]
+    overrideIfInset(
+      CanvasRenderingContext2D.prototype,
+      fillFunction,
       function (otherCtx, original, args) {
         var canvas = otherCtx.canvas
 
         // Need buffer to make sure that shapes draw rihgt on the edge still have shadow.
         var buffer = Math.max(canvas.width, canvas.height)
-        c.width = canvas.width + (2 * buffer)
-        c.height = canvas.height + (2 * buffer)
-
-        ctx.translate(buffer, buffer)
+        resetSize(c, canvas, buffer)
 
         // Draw image.
         original.apply(ctx, args)
@@ -51,13 +83,37 @@
         original.apply(otherCtx, args)
 
         // Draw the actual thing we were drawing.
-        otherCtx.drawImage(c, buffer, buffer, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
+        otherCtx.drawImage(
+          c,
+          buffer,
+          buffer,
+          canvas.width,
+          canvas.height,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        )
 
         swapShadows(ctx, otherCtx)
       }
     )
   }
 
+  function resetSize (destCanvas, srcCanvas, buffer) {
+    var newWidth = srcCanvas.width + 2 * buffer
+    var newHeight = srcCanvas.height + 2 * buffer
+    var newSize = destCanvas.width !== newWidth ||
+      destCanvas.height !== newHeight
+
+    if (newSize) {
+      if (destCanvas.width !== newWidth) destCanvas.width = newWidth
+      if (destCanvas.height !== newHeight) destCanvas.height = newHeight
+      destCanvas.getContext('2d').translate(buffer, buffer)
+    }
+  }
+
+  // Swap shadow properties of ctx1 and ctx2.
   function swapShadows (ctx1, ctx2) {
     var shadowProps = [
       'shadowBlur',
